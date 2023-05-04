56cfdc59bd787c04a65931b35c7f612a99cbd90b
benjaminmaring <benjammaring@gmail.com>
2023-05-03 22:35:59 -0700

    Changes contact background slighly. Fixed JS for image track so that the code is better optimized to smoothly transition over pitcures. Changed the JS to that the slide effect will only happen when dragging on the 'buffer' div, AKA the image track section.

--- shop.js
+++ shop.js
@@ -1,15 +1,15 @@
 const track = document.getElementById("image-track");
 
-window.onmousedown = e => {
+buffer.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
 }
 
-window.onmouseup = () => {
+buffer.onmouseup = () => {
     track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
+    track.dataset.prevPercentage = parseFloat(track.dataset.percentage);
 }
 
-window.onmousemove = e => {
+buffer.onmousemove = e => {
     if (track.dataset.mouseDownAt === "0") return;
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
@@ -18,22 +18,17 @@ window.onmousemove = e => {
     let percentage = (mouseDelta / maxDelta) * -100,
         nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
-    track.dataset.percentage = nextPercentage;
+        nextPercentage = Math.min(nextPercentage, 0);
+        nextPercentage = Math.max(nextPercentage, -90);
 
-    nextPercentage = Math.min(nextPercentage, 0);
-    nextPercentage = Math.max(nextPercentage, -90);
+    track.dataset.percentage = nextPercentage;
 
     track.animate({
         transform: `translate(${nextPercentage}%, 0%)`}, {duration: 1200, fill: "forwards"});
-
-    // The below code is used to add a sliding effect to the pictures as well
-    // while they are moving, however it seem to cause some loading issues
-    // and is not consitently smooth. for now, I am going to leave it out, 
-    // although its cool when it works
     
-    // for (const image of track.getElementsByClassName("img")){
-    //     image.animate({
-    //         objectPosition: `${nextPercentage+100}% center`}, {duration:2000, fill: "forwards"})
-    // }
+    for (const image of track.getElementsByClassName("img")){
+        image.animate({
+            objectPosition: `${nextPercentage+100}% center`}, {duration:2000, fill: "forwards"})
+    }
 
 }// end onmousemove
