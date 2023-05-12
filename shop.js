8eec4a6999de01ffd6445312dd62827c7ba1aa35
BenjaminMaring <benjammaring@gmail.com>
2023-05-11 17:15:17 -0700

    Added a review section to main page

--- shop.js
+++ shop.js
@@ -32,3 +32,32 @@ buffer.onmousemove = e => {
     }
 
 }// end onmousemove
+
+// same methods but for touch events
+//
+//
+//
+
+
+buffer.touchmove = e => {
+    if (track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+          maxDelta = window.innerWidth / 2;
+
+    let percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+        nextPercentage = Math.min(nextPercentage, 0);
+        nextPercentage = Math.max(nextPercentage, -90);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate({
+        transform: `translate(${nextPercentage}%, 0%)`}, {duration: 1200, fill: "forwards"});
+    
+    for (const image of track.getElementsByClassName("img")){
+        image.animate({
+            objectPosition: `${nextPercentage+100}% center`}, {duration:2000, fill: "forwards"})
+    }
+}
